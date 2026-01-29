359c9967cbd36ba2ecd470e9f14a9288ff9e5d69
root <root@MSI.localdomain>
2026-01-29 16:23:03 +0300

    Исправленная версия класса

--- src/datasets/SyntheticDataset.py
+++ src/datasets/SyntheticDataset.py
@@ -1,6 +1,4 @@
 import random
-
-
 import torch
 import sys
 import numpy as np
@@ -9,6 +7,7 @@ import torch.utils.data as data
 from imageio import imread
 from pathlib import Path
 from omegaconf import OmegaConf
+
 
 sys.path.append(str(Path(__file__).parent.parent))
 
@@ -34,12 +33,23 @@ class SyntheticDataset(data.Dataset):
     ):
         self.seed = seed
         
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         torch.set_default_dtype(torch.float32)
-        torch.set_default_device(device)
-        np.random.seed(self.seed)
-        random.seed(self.seed)
-
+  
+        
+        if seed is not None:
+            random.seed(self.seed)
+            np.random.seed(self.seed)
+            torch.manual_seed(self.seed)
+            
+            if torch.cuda.is_available():
+                torch.cuda.manual_seed(self.seed)
+                torch.cuda.manual_seed_all(self.seed)
+                self.cuda_generator = torch.Generator(device='cuda')
+                self.cuda_generator.manual_seed(self.seed)
+            else:
+                self.cuda_generator = None
+                
         self.config = config
         
         self.transform = transform
@@ -50,7 +60,6 @@ class SyntheticDataset(data.Dataset):
         self.ImgAugTransform = ImgAugTransform
         self.customizedTransform = customizedTransform
         
-    
         ######
         self.enable_photo_train = self.config["aug"]["photometric"]["enable"]
         self.enable_homo_train = self.config["aug"]["homographic"]["enable"]
@@ -62,34 +71,36 @@ class SyntheticDataset(data.Dataset):
         self.action = 'training' if task == 'train' else 'validation'
         self.data = self.split_data()[0 if self.action == 'train' else 1]
         
-    def imgPhotometric(self, img):
-        """
-
-        :param img:
-            numpy (H, W)
-        :return:
-        """
-        augmentation = self.ImgAugTransform(**self.config["augmentation"])
-        img = img[:, :, np.newaxis]
-        img = augmentation(img)
-        cusAug = self.customizedTransform()
-        img = cusAug(img, **self.config["augmentation"])
-        return img
-    
-    def get_labels(pnts, H, W):
-        labels = torch.zeros(H, W)
-        pnts_int = torch.min(pnts.round().long(), torch.tensor([[W - 1, H - 1]]).long())
+    @staticmethod
+    def get_labels(pnts, H, W, device=None):
+        pnts = pnts.to(device)
+        
+        labels = torch.zeros(H, W, device=device)
+        pnts_int = torch.min(pnts.round().long(), 
+                           torch.tensor([[W - 1, H - 1]], device=device).long())
         labels[pnts_int[:, 1], pnts_int[:, 0]] = 1
         return labels
     
-
-    def get_label_res(self, H, W, pnts):
+    def get_label_res(self, H, W, pnts, device=None):
         """Создание карты субпиксельных смещений"""
-        labels_res = torch.zeros(H, W, 2)
+        labels_res = torch.zeros(H, W, 2, device=device)
         pnts_int = pnts.round().long()
+        
+        pnts = pnts.to(device)
+        pnts_int = pnts_int.to(device)
+        
         labels_res[pnts_int[:, 1], pnts_int[:, 0], :] = pnts - pnts.round()
         labels_res = labels_res.transpose(1, 2).transpose(0, 1)
         return labels_res
+        
+    def imgPhotometric(self, img):
+        augmentation = self.ImgAugTransform(**self.config["aug"])
+        img = img[:, :, np.newaxis]
+        img = augmentation(img)
+        cusAug = self.customizedTransform()
+        #img = cusAug(img, **self.config["aug"])
+        img = cusAug(img)
+        return img
     
     def split_data(self):
         random_state = np.random.RandomState(self.seed)
@@ -101,10 +112,10 @@ class SyntheticDataset(data.Dataset):
 
         for primitive in all_primitives:
             for el in range(20):
-                dir_path = base_dir / Path(primitive, f"{el:02d}")
+                dir_path = base_dir / Path(primitive, f"00{el:02d}")
                 for ind in range(1000):
-                    img_path = dir_path / f'00{ind:04d}.png'
-                    pts_path = dir_path / f'00{ind:04d}.npy'
+                    img_path = dir_path / f'{el * 1000 + ind:06d}.png'
+                    pts_path = dir_path / f'{el * 1000 + ind:06d}.npy'
                     all_files.append((img_path, pts_path))
                     
         total_files = len(all_files)
@@ -114,7 +125,6 @@ class SyntheticDataset(data.Dataset):
         train_ind = random_state.choice(all_indices, size=train_count, replace=False)
         train_ind = set(train_ind)
         val_ind = set(all_indices) - train_ind
-
 
         train_data = [all_files[i] for i in train_ind]
         val_data = [all_files[i] for i in val_ind]
@@ -130,30 +140,14 @@ class SyntheticDataset(data.Dataset):
         H, W = img.shape[0], img.shape[1]
         
         
-        # labels_2D = self.get_labels(pts, H, W)
-        # labels_2D = torch.from_numpy(labels_2D)
-        
-        
         if (self.config["aug"]["photometric"]["enable_train"] and self.action == "training") or (
             self.config["aug"]["photometric"]["enable_val"] and self.action == "validation"
         ):
             img = self.imgPhotometric(img)
-        else:
-            pass
         
         apply_homography = (self.config["aug"]["homographic"]["enable_train"] and self.action == "training")\
                             or (self.config["aug"]["homographic"]["enable_val"] and self.action == "validation")
-        # if not apply_homography:
-        #     img = img[:, :, np.newaxis]
-           
-        #     if self.transform is not None:
-        #         img = self.transform(img)
-                
-        #     img = torch.from_numpy(img)
-        #     valid_mask = self.compute_valid_mask(torch.tensor([H, W]), inv_homography=torch.eye(3))
-            
-        #     labels_res = self.get_label_res(H, W, pts)
-           
+        
         if apply_homography:
             homography = self.sample_homography(
                 np.array([2, 2]),
@@ -163,14 +157,20 @@ class SyntheticDataset(data.Dataset):
             homography = np.linalg.inv(homography)
             homography = torch.tensor(homography).float()
             inv_homography = homography.inverse()
-            img = torch.from_numpy(img)
-            warped_img = self.inv_warp_image(img.squeeze(), inv_homography, mode="bilinear")
+            
+            device = self.device
+            
+            img_tensor = torch.from_numpy(img).float()
+            warped_img = self.inv_warp_image(img_tensor.squeeze(), inv_homography, mode="bilinear")
             warped_img = warped_img.squeeze().numpy()
             warped_img = warped_img[:, :, np.newaxis]
 
-            warped_pts = self.warp_points(pts, homography_scaling(homography, H, W))
+            homography_scaled = homography_scaling(homography, H, W)
+            
+            pts_tensor = torch.from_numpy(pts).float()
+            warped_pts = self.warp_points(pts_tensor, homography_scaled)
             warped_pts = filter_points(warped_pts, torch.tensor([W, H]))
-
+            
             if self.transform is not None:
                 warped_img = self.transform(warped_img)
             
@@ -178,41 +178,33 @@ class SyntheticDataset(data.Dataset):
                 torch.tensor([H, W]),
                 inv_homography=inv_homography,
                 erosion_radius=self.config["aug"]["homographic"]["valid_border_margin"],
-            )  # can set to other value
-
-            labels_2D = self.get_labels(warped_pts, H, W)
-            warped_labels_res = self.get_label_res(H, W, warped_pts)
+            ).to(device)
             
+            # Используем правильный device для меток
+            labels_2D = self.get_labels(warped_pts, H, W, device=device)
+            warped_labels_res = self.get_label_res(H, W, warped_pts, device=device)
+            
+            img_tensor = torch.from_numpy(img).float()
+            if img_tensor.dim() == 2:
+                img_tensor = img_tensor.unsqueeze(0)
+            img_tensor = img_tensor.to(device)
+            
+            warped_img_tensor = torch.from_numpy(warped_img).float()
+            if warped_img_tensor.dim() == 2:
+                warped_img_tensor = warped_img_tensor.unsqueeze(0)
+            elif warped_img_tensor.dim() == 3 and warped_img_tensor.shape[2] == 1:
+                warped_img_tensor = warped_img_tensor.permute(2, 0, 1)
+            warped_img_tensor = warped_img_tensor.to(device)
+        
         sample = (
-            img,
+            img_tensor,
             labels_2D,
-            homography,
             valid_mask,
-            warped_img,
-            warped_pts,
+            warped_img_tensor,
             warped_labels_res
         )
-        # else:
-        #     sample = (
-        #         img, 
-        #         valid_mask,
-        #         label_res
-        #     )
         
-
         return sample
     
     def __len__(self):
         return len(self.data)
-    
-
-  
-if __name__ == "__main__":
-    params_path = Path(__file__).parent.parent / 'params.yaml'
-    cfg = OmegaConf.load(str(params_path))
-    
-    training_cfg = cfg['prepare_synthetic_dataset']
-    dataset = SyntheticDataset(**training_cfg)
-    
-    train_data, val_data = dataset.split_data()
-    print(train_data[0], val_data[0])
