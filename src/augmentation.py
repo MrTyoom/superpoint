at: +refactoring, fix loss calculation

src/augmentation.py
@@ -1,12 +1,10 @@
-from typing import NewType
-
 import cv2
 import numpy as np
 from imgaug import augmenters as iaa
 from numpy.random import rand, randint
 from omegaconf import DictConfig
 
-TupleInt = NewType("TupleInt", tuple[int, int])
+from src.types import TupleInt
 
 
 def get_photometric_augmentation(cfg: DictConfig):
@@ -36,14 +34,14 @@ def get_photometric_augmentation(cfg: DictConfig):
 def get_ellipse_axes(dim: int) -> TupleInt:
     ax = int(max(rand() * dim, dim / 5))
     ay = int(max(rand() * dim, dim / 5))
-    return TupleInt((ax, ay))
+    return ax, ay
 
 
 def get_ellipse_center(img_shape: TupleInt, axes: TupleInt) -> TupleInt:
     max_rad = max(axes)
     x_cord = randint(max_rad, img_shape[1] - max_rad)
     y_cord = randint(max_rad, img_shape[0] - max_rad)
-    return TupleInt((x_cord, y_cord))
+    return x_cord, y_cord
 
 
 def blur_mask(kernel_size_range, mask):
@@ -77,12 +75,12 @@ def generate_mask(cfg: DictConfig, img_shape: TupleInt):
 
         cv2.ellipse(
             mask,
-            get_ellipse_center(img_shape, TupleInt(axes)),
+            get_ellipse_center(img_shape, axes),
             axes,
             np.random.rand() * min_angle,
             0,
             max_angle,
-            color,
+            color,  # type: ignore
             -1,
         )
 

src/homography/apply.py
@@ -2,7 +2,8 @@
 
 import torch
 from torch.nn.functional import grid_sample
-from torch.types import Tensor
+
+from src.types import Tensor
 
 TWO = 2.0
 
@@ -119,11 +120,11 @@ def homography_scaling_torch(homography: Tensor, height: int, width: int) -> Ten
     return homography
 
 
-def filter_points(points: Tensor, shape: Tensor, return_mask: bool = False) -> Tensor | tuple[Tensor, Tensor]:
+def filter_points(points: Tensor, shape: Tensor) -> Tensor:
     points = points.float()
     shape = shape.float()
+
     mask = (points >= 0) * (points <= shape - 1)
     mask = torch.prod(mask, dim=-1) == 1
-    if return_mask:
-        return points[mask], mask
+
     return points[mask]

src/homography/homography_utils.py
@@ -3,16 +3,15 @@
 import cv2
 import numpy as np
 import torch
-from numpy.typing import NDArray
 from omegaconf import DictConfig
-from torch.types import Tensor
 
 from src.homography.apply import inv_warp_image_batch
 from src.homography.generation import get_corners
 from src.homography.transforms import perspective_transform
+from src.types import Array, Tensor
 
 
-def sample_homography(cfg: DictConfig, shape: NDArray, shift: int) -> tuple[Tensor, Tensor]:
+def sample_homography(cfg: DictConfig, shape: Array, shift: int) -> tuple[Tensor, Tensor]:
     """
     Генерирует случайную гомографию
     """
@@ -22,11 +21,12 @@ def sample_homography(cfg: DictConfig, shape: NDArray, shift: int) -> tuple[Tens
     pts1 *= shape[np.newaxis]
     pts2 *= shape[np.newaxis]
 
-    inv_homography = cv2.getPerspectiveTransform(np.float32(pts1 + shift), np.float32(pts2 + shift))
+    inv_homography = cv2.getPerspectiveTransform(np.float32(pts1 + shift), np.float32(pts2 + shift))  # type: ignore
     homography = np.linalg.inv(inv_homography)
 
     homography = torch.tensor(homography).float()
     inv_homography = torch.tensor(inv_homography).float()
+
     return homography, inv_homography
 
 
@@ -43,9 +43,9 @@ def compute_valid_mask(
         inv_homography = inv_homography.view(-1, 3, 3)
 
     batch_size = inv_homography.shape[0]
-    mask = torch.ones(batch_size, 1, image_shape[0], image_shape[1]).to(device)
+    mask = torch.ones(batch_size, 1, image_shape[0], image_shape[1]).to(device)  # type: ignore
     mask = inv_warp_image_batch(mask, inv_homography, device=device, mode="nearest")
-    mask = mask.view(batch_size, image_shape[0], image_shape[1])
+    mask = mask.view(batch_size, image_shape[0], image_shape[1])  # type: ignore
     mask = mask.cpu().numpy()
 
     if erosion_radius > 0:

src/homography/transforms.py
@@ -2,14 +2,14 @@
 
 import numpy as np
 from numpy.random import randint, uniform
-from numpy.typing import NDArray
 from omegaconf import DictConfig
 from scipy.stats import truncnorm
 
 from src.homography.generation import add_perspective
+from src.types import Array
 
 
-def calc_scales_and_center(cfg, pts: NDArray, std_trunc: int = 2) -> tuple[NDArray, NDArray]:
+def calc_scales_and_center(cfg, pts: Array, std_trunc: int = 2) -> tuple[Array, Array]:
     """Вычисляет возможные масштабы и центр для масштабирования"""
     scales = truncnorm(-std_trunc, std_trunc, loc=1, scale=cfg.scaling_amplitude / 2).rvs(cfg.n_scales)
     scales = np.concatenate((np.array([1]), scales), axis=0)
@@ -17,7 +17,7 @@ def calc_scales_and_center(cfg, pts: NDArray, std_trunc: int = 2) -> tuple[NDArr
     return scales, center
 
 
-def get_scaling(cfg: DictConfig, pts: NDArray) -> NDArray:
+def get_scaling(cfg: DictConfig, pts: Array) -> Array:
     """Применяет случайное масштабирование к углам"""
     scales, center = calc_scales_and_center(cfg, pts)
 
@@ -36,7 +36,7 @@ def get_scaling(cfg: DictConfig, pts: NDArray) -> NDArray:
     return pts[idx]
 
 
-def get_translation(cfg: DictConfig, pts: NDArray) -> NDArray:
+def get_translation(cfg: DictConfig, pts: Array) -> Array:
     """Применяет случайный сдвиг к углам"""
     t_min, t_max = np.min(pts, axis=0), np.min(1 - pts, axis=0)
 
@@ -50,7 +50,7 @@ def get_translation(cfg: DictConfig, pts: NDArray) -> NDArray:
     return pts + np.array([x_rnd, y_rnd]).T
 
 
-def calc_rotation(cfg: DictConfig, pts: NDArray):
+def calc_rotation(cfg: DictConfig, pts: Array):
     """Вычисляет матрицы поворота для всех возможных углов"""
     angles = np.linspace(-cfg.max_angle, cfg.max_angle, num=cfg.n_angles)
     angles = np.concatenate((angles, np.array([0])), axis=0)
@@ -66,7 +66,7 @@ def calc_rotation(cfg: DictConfig, pts: NDArray):
     return center, rot_mat
 
 
-def get_rotation(cfg: DictConfig, pts: NDArray) -> NDArray:
+def get_rotation(cfg: DictConfig, pts: Array) -> Array:
     """Применяет случайный поворот к углам"""
     center, rot_mat = calc_rotation(cfg, pts)
 
@@ -84,7 +84,7 @@ def get_rotation(cfg: DictConfig, pts: NDArray) -> NDArray:
     return pts[idx]
 
 
-def perspective_transform(cfg: DictConfig, pts: NDArray) -> NDArray:
+def perspective_transform(cfg: DictConfig, pts: Array) -> Array:
     """Последовательно применяет все трансформации к углам"""
     if cfg.perspective:
         pts = add_perspective(cfg, pts)

src/metrics.py
@@ -1,38 +1,65 @@
+import numpy as np
 import torch
 
 from src.train_utils.d2s import DepthToSpace
-from src.train_utils.train_utils import getPtsFromHeatmap
+from src.train_utils.train_utils import get_pts_from_heatmap
+from src.types import Tensor
 
 
-def get_heatmap(semi):
+def get_heatmap(semi: Tensor) -> Tensor:
     semi_soft = torch.nn.functional.softmax(semi, dim=1)
     nodust = semi_soft[:, :-1, :, :]
 
     depth_to_space = DepthToSpace(8)
     heatmap = depth_to_space(nodust)
+
     return heatmap
 
 
-def precisionRecall(pred, labels):
-    offset = 10**-6
-    precision = torch.sum(pred * labels) / (torch.sum(pred) + offset)
-    recall = torch.sum(pred * labels) / (torch.sum(labels) + offset)
-    return {"precision": precision, "recall": recall}
+def get_precision_recall(pred: Tensor, labels: Tensor) -> tuple[Tensor, Tensor]:
+    OFFSET = 10**-6
+
+    precision = torch.sum(pred * labels) / (torch.sum(pred) + OFFSET)
+    recall = torch.sum(pred * labels) / (torch.sum(labels) + OFFSET)
+
+    return precision, recall
 
 
-def calculate_precisionRecall(batch, heatmap, lab_mask, detection_threshold, nms_dist):
-    labels, masks = lab_mask
+def calculate_precision_recall(
+    heatmap: Tensor, labels: Tensor, masks: Tensor, detection_threshold: float, nms_dist: int
+) -> tuple[Tensor, Tensor]:
 
-    heatmap_np = heatmap[batch, 0]
-    heatmap_np = heatmap_np.cpu().numpy()
-    pts_nms = getPtsFromHeatmap(heatmap_np, detection_threshold, nms_dist)  # (2, N)
+    heatmap_np = heatmap.cpu().numpy()
+    pts_nms = get_pts_from_heatmap(heatmap_np, detection_threshold, nms_dist)
 
-    pred_map = torch.zeros_like(labels[batch, 0]).cpu()
+    pred_map = torch.zeros_like(labels).cpu()
     pts_nms = torch.from_numpy(pts_nms).long()
+
     if pts_nms.shape[1] > 0:
-        pred_map[pts_nms[1, :].long(), pts_nms[0, :].long()] = 1
+        pred_map[pts_nms[1, :], pts_nms[0, :]] = 1
 
-    gt_map = (labels[batch, 0] * masks[batch, 0]).cpu()
+    gt_map = (labels * masks).cpu()
 
-    pr = precisionRecall(pred_map, gt_map)
-    return pr
+    precision, recall = get_precision_recall(pred_map, gt_map)
+
+    return precision, recall
+
+
+def batch_precision_recall(
+    heatmap: Tensor, labels: Tensor, masks: Tensor, detection_threshold: float, nms_dist: int
+) -> tuple[float, float]:
+
+    batch_size = heatmap.shape[0]
+
+    batch_precision = np.zeros(batch_size)
+    batch_recall = np.zeros(batch_size)
+
+    for it in range(batch_size):
+        precision, recall = calculate_precision_recall(
+            heatmap[it, 0], labels[it, 0], masks[it, 0], detection_threshold, nms_dist
+        )
+
+        batch_precision[it] = precision
+        batch_recall[it] = recall
+
+    return batch_precision.mean(), batch_recall.mean()

src/superpoint.py
@@ -1,20 +1,14 @@
-from typing import Any
-
 import torch
 from lightning import LightningModule
-from omegaconf import DictConfig
 from torch.nn import BatchNorm2d, BCELoss, Conv2d, MaxPool2d, Module, ReLU
-from torch.types import Tensor
 from torchmetrics import MeanMetric
 
 from src.loss import loss_calculation
-from src.metrics import calculate_precisionRecall, get_heatmap
-
-EPS = 1e-8
-SEMI = "semi"
+from src.metrics import batch_precision_recall, get_heatmap
+from src.types import Tensor
 
 
-class SuperPoint(Module):
+class SuperPoint(Module):  # noqa: WPS230
     def __init__(self):
         super().__init__()
 
@@ -29,28 +23,28 @@ class SuperPoint(Module):
         self.relu = ReLU(inplace=True)
         self.pool = MaxPool2d(kernel_size=2, stride=2)
 
-        self._bn1a = BatchNorm2d(c1)
-        self._bn1b = BatchNorm2d(c1)
-        self._bn2a = BatchNorm2d(c2)
-        self._bn2b = BatchNorm2d(c2)
-        self._bn3a = BatchNorm2d(c3)
-        self._bn3b = BatchNorm2d(c3)
-        self._bn4a = BatchNorm2d(c4)
-        self._bn4b = BatchNorm2d(c4)
-        self._bnPa = BatchNorm2d(c5)
-        self._bnPb = BatchNorm2d(det_h)
-        self._bnDa = BatchNorm2d(c5)
-        self._bnDb = BatchNorm2d(d1)
+        self.bn1a = BatchNorm2d(c1)
+        self.bn1b = BatchNorm2d(c1)
+        self.bn2a = BatchNorm2d(c2)
+        self.bn2b = BatchNorm2d(c2)
+        self.bn3a = BatchNorm2d(c3)
+        self.bn3b = BatchNorm2d(c3)
+        self.bn4a = BatchNorm2d(c4)
+        self.bn4b = BatchNorm2d(c4)
+        self.bnPa = BatchNorm2d(c5)
+        self.bnPb = BatchNorm2d(det_h)
+        self.bnDa = BatchNorm2d(c5)
+        self.bnDb = BatchNorm2d(d1)
 
         # Encoder.
-        self._conv1a = Conv2d(1, c1, 3, padding=1)
-        self._conv1b = Conv2d(c1, c1, 3, padding=1)
-        self._conv2a = Conv2d(c1, c2, 3, padding=1)
-        self._conv2b = Conv2d(c2, c2, 3, padding=1)
-        self._conv3a = Conv2d(c2, c3, 3, padding=1)
-        self._conv3b = Conv2d(c3, c3, 3, padding=1)
-        self._conv4a = Conv2d(c3, c4, 3, padding=1)
-        self._conv4b = Conv2d(c4, c4, 3, padding=1)
+        self.conv1a = Conv2d(1, c1, 3, padding=1)
+        self.conv1b = Conv2d(c1, c1, 3, padding=1)
+        self.conv2a = Conv2d(c1, c2, 3, padding=1)
+        self.conv2b = Conv2d(c2, c2, 3, padding=1)
+        self.conv3a = Conv2d(c2, c3, 3, padding=1)
+        self.conv3b = Conv2d(c3, c3, 3, padding=1)
+        self.conv4a = Conv2d(c3, c4, 3, padding=1)
+        self.conv4b = Conv2d(c4, c4, 3, padding=1)
 
         # Detector Head.
         self.convPa = Conv2d(c4, c5, kernel_size=3, padding=1)
@@ -62,39 +56,40 @@ class SuperPoint(Module):
 
     def forward(self, batch: Tensor, descriptor=True):
         # Encoder
-        x1 = self.relu(self._bn1a(self._conv1a(batch)))
-        x2 = self.relu(self._bn1b(self._conv1b(x1)))
+        x1 = self.relu(self.bn1a(self.conv1a(batch)))
+        x2 = self.relu(self.bn1b(self.conv1b(x1)))
         x3 = self.pool(x2)
 
-        x4 = self.relu(self._bn2a(self._conv2a(x3)))
-        x5 = self.relu(self._bn2b(self._conv2b(x4)))
+        x4 = self.relu(self.bn2a(self.conv2a(x3)))
+        x5 = self.relu(self.bn2b(self.conv2b(x4)))
         x6 = self.pool(x5)
 
-        x7 = self.relu(self._bn3a(self._conv3a(x6)))
-        x8 = self.relu(self._bn3b(self._conv3b(x7)))
+        x7 = self.relu(self.bn3a(self.conv3a(x6)))
+        x8 = self.relu(self.bn3b(self.conv3b(x7)))
         x9 = self.pool(x8)
 
-        x10 = self.relu(self._bn4a(self._conv4a(x9)))
-        feats = self.relu(self._bn4b(self._conv4b(x10)))
+        x10 = self.relu(self.bn4a(self.conv4a(x9)))
+        feats = self.relu(self.bn4b(self.conv4b(x10)))
 
         # Detector Head
-        cPa = self.relu(self._bnPa(self.convPa(feats)))
-        semi = self._bnPb(self.convPb(cPa))
+        cPa = self.relu(self.bnPa(self.convPa(feats)))
+        semi = self.bnPb(self.convPb(cPa))
 
         if not descriptor:
             return semi
 
         # Descriptor Head
-        cDa = self.relu(self._bnDa(self.convDa(feats)))
-        desc = self._bnDb(self.convDb(cDa))
+        cDa = self.relu(self.bnDa(self.convDa(feats)))
+        desc = self.bnDb(self.convDb(cDa))
 
         dn = torch.norm(desc, p=2, dim=1)
         desc = desc.div(torch.unsqueeze(dn, 1))
+
         return semi, desc
 
 
 class MagicPointLightning(LightningModule):
-    def __init__(self, cfg: DictConfig) -> None:
+    def __init__(self, cfg: dict) -> None:
         super().__init__()
 
         self._descriptor = False
@@ -114,11 +109,11 @@ class MagicPointLightning(LightningModule):
 
     def training_step(self, sample: tuple[Tensor, ...]) -> Tensor:
         img, masks, labels = sample
-
         semi = self(img)
-        loss = loss_calculation(semi, labels, masks, self._criterion, self.hparams.cell_size)
 
+        loss = loss_calculation(semi, labels, masks, self._criterion, self.hparams.cell_size)  # type: ignore
         self.train_loss(loss)
+
         return loss
 
     def on_train_epoch_end(self) -> None:
@@ -128,45 +123,37 @@ class MagicPointLightning(LightningModule):
 
     def validation_step(self, sample: tuple[Tensor, ...]) -> None:
         img, masks, labels = sample
-
         semi = self(img)
-        loss = loss_calculation(semi, labels, masks, self._criterion, self.hparams.cell_size)
 
+        loss = loss_calculation(semi, labels, masks, self._criterion, self.hparams.cell_size)  # type: ignore
         self.val_loss(loss)
 
         heatmap = get_heatmap(semi)
 
-        batch_precision = []
-        batch_recall = []
-        lab_mask = (labels, masks)
+        precision, recall = batch_precision_recall(
+            heatmap,
+            labels,
+            masks,
+            self.hparams.detection_threshold,
+            self.hparams.nms_dist,  # type: ignore
+        )
 
-        for batch in range(semi.shape[0]):
-            pr = calculate_precisionRecall(
-                batch, heatmap, lab_mask, self.hparams.detection_threshold, self.hparams.nms_dist
-            )
-            batch_precision.append(pr["precision"])
-            batch_recall.append(pr["recall"])
-
-        if len(batch_precision) > 0:
-            avg_batch_precision = sum(batch_precision) / len(batch_precision)
-            avg_batch_recall = sum(batch_recall) / len(batch_recall)
-
-            self.val_precision.update(torch.tensor(avg_batch_precision))
-            self.val_recall.update(torch.tensor(avg_batch_recall))
+        self.val_precision.update(precision)
+        self.val_recall.update(recall)
 
     def on_validation_epoch_end(self) -> None:
         avg_loss = self.val_loss.compute()
         avg_precision = self.val_precision.compute()
         avg_recall = self.val_recall.compute()
 
-        self.log("val/epoch_loss", avg_loss, on_step=False, on_epoch=True)
-        self.log("val/epoch_precision", avg_precision, on_step=False, on_epoch=True)
-        self.log("val/epoch_recall", avg_recall, on_step=False, on_epoch=True)
+        self.log("val/loss", avg_loss, on_step=False, on_epoch=True)
+        self.log("val/precision", avg_precision, on_step=False, on_epoch=True)
+        self.log("val/recall", avg_recall, on_step=False, on_epoch=True)
 
         self.val_loss.reset()
         self.val_precision.reset()
         self.val_recall.reset()
 
-    def configure_optimizers(self) -> dict[str, Any]:
-        optimizer = torch.optim.Adam(self._net.parameters(), lr=self.hparams.learning_rate, betas=(0.9, 0.999))
+    def configure_optimizers(self):  # type: ignore
+        optimizer = torch.optim.Adam(self._net.parameters(), lr=self.hparams.learning_rate)  # type: ignore
         return optimizer

src/synthetic_loader.py
@@ -1,3 +1,4 @@
+import random
 from pathlib import Path
 
 import cv2
@@ -5,12 +6,12 @@ import numpy as np
 import torch
 from lightning import LightningDataModule
 from omegaconf import DictConfig
-from torch.types import Tensor
 from torch.utils.data import DataLoader, Dataset
 
 from src.augmentation import PhotometricAugmentation
 from src.homography.apply import filter_points, homography_scaling_torch, inv_warp_image, warp_points
 from src.homography.homography_utils import compute_valid_mask, sample_homography
+from src.types import Tensor
 
 MAX_PIXEL = 255.0
 
@@ -42,7 +43,7 @@ class SyntheticDataset(Dataset):
         return len(self.image_files)
 
     def __getitem__(self, index: int) -> tuple[Tensor, ...]:
-        src_image = cv2.imread(self.image_files[index], cv2.IMREAD_GRAYSCALE)
+        src_image = cv2.imread(self.image_files[index], cv2.IMREAD_GRAYSCALE)  # type: ignore
         src_points = np.load(self.annot_files[index])
 
         height, width = src_image.shape
@@ -79,8 +80,7 @@ class SyntheticDataset(Dataset):
 
         labels = get_labels(warped_pts, height, width)
 
-        sample = (warped_img, masks, labels)
-        return sample
+        return warped_img, masks, labels
 
 
 class Loader(LightningDataModule):
@@ -93,9 +93,9 @@ class Loader(LightningDataModule):
         self.save_hyperparameters(logger=False)
 
     def train_dataloader(self) -> DataLoader:
-        cfg = self.hparams.cfg
+        cfg = self.hparams.cfg  # type: ignore
         return DataLoader(
-            dataset=self.train_dataset,
+            dataset=self.train_dataset,  # type: ignore
             batch_size=cfg.train_batch_size,
             num_workers=cfg.num_workers,
             drop_last=True,
@@ -103,9 +103,9 @@ class Loader(LightningDataModule):
         )
 
     def val_dataloader(self) -> DataLoader:
-        cfg = self.hparams.cfg
+        cfg = self.hparams.cfg  # type: ignore
         return DataLoader(
-            dataset=self.val_dataset,
+            dataset=self.val_dataset,  # type: ignore
             batch_size=cfg.val_batch_size,
             num_workers=cfg.num_workers,
             shuffle=False,
@@ -116,19 +116,18 @@ class Loader(LightningDataModule):
         if stage == "fit":
             train_files, val_files = self.split_files()
 
-            aug_cfg = self.hparams.cfg.augmentation
+            aug_cfg = self.hparams.cfg.augmentation  # type: ignore
 
             self.train_dataset = SyntheticDataset(train_files, aug_cfg, "train")
             self.val_dataset = SyntheticDataset(val_files, aug_cfg, "val")
 
     def split_files(self) -> tuple[list[Path], list[Path]]:
-        cfg = self.hparams.cfg
+        cfg = self.hparams.cfg  # type: ignore
 
         files = list(Path(cfg.data_dir).glob("**/*.png"))
         files = [cur_file for cur_file in files if cur_file.with_suffix(".npy").exists()]
 
-        rng = np.random.RandomState(cfg.seed)
-        rng.shuffle(files)
+        random.shuffle(files)
 
         train_size = cfg.train_size
         num_train_files = int(len(files) * train_size)

src/train_utils/train_utils.py
@@ -1,14 +1,9 @@
 import numpy as np
 
+from src.types import Array
 
-def getPtsFromHeatmap(heatmap, conf_thresh, nms_dist):
-    """
-    :param self:
-    :param heatmap:
-        np (H, W)
-    :return:
-    """
 
+def get_pts_from_heatmap(heatmap: Array, conf_thresh: float, nms_dist: int) -> Array:
     border_remove = 4
 
     width, width = heatmap.shape[0], heatmap.shape[1]
@@ -16,13 +11,17 @@ def getPtsFromHeatmap(heatmap, conf_thresh, nms_dist):
 
     if len(xs) == 0:
         return np.zeros((3, 0))
+
     pts = np.zeros((3, len(xs)))  # Populate point data sized 3xN.
     pts[0, :] = ys
     pts[1, :] = xs
     pts[2, :] = heatmap[xs, ys]
+
     pts, _ = nms_fast(pts, width, width, dist_thresh=nms_dist)  # Apply NMS.
+
     inds = np.argsort(pts[2, :])
     pts = pts[:, inds[::-1]]  # Sort by confidence.
+
     # Remove points along border.
     bord = border_remove
 
@@ -32,11 +31,13 @@ def getPtsFromHeatmap(heatmap, conf_thresh, nms_dist):
     toremoveW = np.logical_or(pts[0, :] < bord, width_bord)
     toremoveH = np.logical_or(pts[1, :] < bord, height_bord)
     toremove = np.logical_or(toremoveW, toremoveH)
+
     pts = pts[:, ~toremove]
+
     return pts
 
 
-def nms_fast(in_corners, height, width, dist_thresh):
+def nms_fast(in_corners: Array, height: int, width: int, dist_thresh: int):
     """
     Run a faster approximate Non-Max-Suppression on numpy corners shaped:
       3xN [x_i,y_i,conf_i]^T
@@ -60,37 +61,45 @@ def nms_fast(in_corners, height, width, dist_thresh):
     """
     grid = np.zeros((height, width)).astype(int)  # Track NMS data.
     inds = np.zeros((height, width)).astype(int)  # Store indices of points.
+
     # Sort by confidence and round to nearest int.
     inds1 = np.argsort(-in_corners[2, :])
     corners = in_corners[:, inds1]
     rcorners = corners[:2, :].round().astype(int)  # Rounded corners.
+
     # Check for edge case of 0 or 1 corners.
     if rcorners.shape[1] == 0:
         return np.zeros((3, 0)).astype(int), np.zeros(0).astype(int)
+
     if rcorners.shape[1] == 1:
         out = np.vstack((rcorners, in_corners[2])).reshape(3, 1)
         return out, np.zeros((1)).astype(int)
+
     # Initialize the grid.
     for idx, rc in enumerate(rcorners.T):
         grid[rcorners[1, idx], rcorners[0, idx]] = 1
         inds[rcorners[1, idx], rcorners[0, idx]] = idx
+
     # Pad the border of the grid, so that we can NMS points near the border.
     pad = dist_thresh
     grid = np.pad(grid, ((pad, pad), (pad, pad)), mode="constant")
+
     # Iterate through points, highest to lowest conf, suppress neighborhood.
     count = 0
+
     for idx, rc in enumerate(rcorners.T):
         # Account for top and left padding.
         pt = (rc[0] + pad, rc[1] + pad)
+
         if grid[pt[1], pt[0]] == 1:  # If not yet suppressed.
             x_start = pt[1] - pad
             x_end = pt[1] + pad + 1
             y_start = pt[0] - pad
             y_end = pt[0] + pad + 1
-
             grid[x_start:x_end, y_start:y_end] = 0
             grid[pt[1], pt[0]] = -1
             count += 1
+
     # Get all surviving -1's and return sorted array of remaining corners.
     keepy, keepx = np.where(grid == -1)
     keepy, keepx = keepy - pad, keepx - pad
@@ -100,4 +109,5 @@ def nms_fast(in_corners, height, width, dist_thresh):
     inds2 = np.argsort(-out_vals)
     out = out[:, inds2]
     out_inds = inds1[inds_keep[inds2]]
+
     return out, out_inds