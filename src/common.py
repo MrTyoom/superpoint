df31139ae24c8f18b045bcd08295510fa02946bf
root <root@MSI.localdomain>
2026-03-19 20:16:03 +0300

    as: added scripts for downloading satellite data

--- src/common.py
+++ src/common.py
@@ -2,10 +2,10 @@ import shutil
 from pathlib import Path
 
 
-def make_dir(path: str | Path) -> Path:
+def make_dir(path: str | Path, delete_if_exist: bool) -> Path:
     path = Path(path)
 
-    if path.is_dir():
+    if delete_if_exist and path.is_dir():
         shutil.rmtree(path)
 
     path.mkdir(parents=True, exist_ok=True)
