ddefde94ddf17ac4eb359184532cf0a46331731b
MrTyoom <artem.sorokin04@gmail.com>
2026-03-30 15:17:58 +0300

    as: fix

--- src/common.py
+++ src/common.py
@@ -2,7 +2,7 @@ import shutil
 from pathlib import Path
 
 
-def make_dir(path: str | Path, delete_if_exist: bool = True) -> Path:
+def make_dir(path: str | Path, delete_if_exist: bool = False) -> Path:
     path = Path(path)
 
     if delete_if_exist and path.is_dir():
